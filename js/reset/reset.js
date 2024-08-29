6526c56cc1142d276ebcef222ff405a9a349ff5c
AwesomeSeal <Supersayon40@gmail.com>
2024-08-29 10:59:56 -0500

    Add files via upload
    Initial fork, including changes from initial addition of Reset

---
+++ js/reset/reset.js
@@ -0,0 +1,17 @@
+let GameWins = {
+  amount() {
+    return player.gameWins;
+  },
+  multiplier() {
+    return Math.pow(2, this.amount());
+  },
+  increment() {
+	if (confirm('Do you really want to reset the game? You will lose all your progress, and get a 2x multiplier to all generators, and a speed increase to boost power, chroma, power generation, and permenance?')) {
+    Notifications.notify('Game has been reset!', 'saveLoad');
+    player.gameWins++;
+	let oldWins = player.gameWins
+    Saving.resetGame();
+	player.gameWins = oldWins
+	}
+  },
+}
